05c2da4b4ed551239387eda8548d2e6dfac86647
Paavas Bhasin <paavas22@gmail.com>
2020-04-03 16:53:22 +0530

    Lecture 15

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,26 +1,34 @@
 'use strict';
 
-// function square(x) {
-//     return x*x;
-// };
+var add = function add(a, b) {
+    //console.log(arguments);
+    return a + b;
+};
+console.log(add(55, 1, 1001));
 
-// console.log(square(3));
+var user = {
+    name: 'Paavas',
+    cities: ['New Delhi', 'Nashville', 'Atlanta', 'Edinburgh'],
+    printPlacesLived: function printPlacesLived() {
+        var _this = this;
 
-// // const squareArrow = (x) => {
-// //     return x*x;
-// // }
+        return this.cities.map(function (city) {
+            return _this.name + ' has lived in ' + city;
+        });
+    }
+};
+console.log(user.printPlacesLived());
 
-// const squareArrow = (x) => x*x;
+var multiplier = {
+    numbers: [1, 2, 3, 4],
+    multiplyBy: 8,
+    multiply: function multiply() {
+        var _this2 = this;
 
-// console.log(squareArrow(9));
-
-var getFirstName = function getFirstName(name) {
-    return name.split(' ')[0];
+        return this.numbers.map(function (number) {
+            return number * _this2.multiplyBy;
+        });
+    }
 };
 
-var getFirstName2 = function getFirstName2(name) {
-    return name.split(' ')[0];
-};
-
-console.log(getFirstName('Paavas Bhasin'));
-console.log(getFirstName2('Aniket Joshi'));
+console.log(multiplier.multiply());
