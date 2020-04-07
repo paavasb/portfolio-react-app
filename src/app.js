ed1949aa26c2b072cda0bcd0c07c8d77aa76f1e8
Paavas Bhasin <paavas22@gmail.com>
2020-04-07 23:55:45 +0530

    Lecture 45

--- src/app.js
+++ src/app.js
@@ -10,10 +10,23 @@ class IndecisionApp extends React.Component {
         }
     }
     componentDidMount() {
-        console.log('fetching data');
+        try {
+            const json = localStorage.getItem('options');
+            const options = JSON.parse(json);
+    
+            if(options) {
+                this.setState(() => ({options}));
+            }
+        } catch(e) {
+            //Do nothing
+        }
     }
     componentDidUpdate(prevProps, prevState) {
-        console.log('saving data');
+        if(prevState.options.length !== this.state.options.length) {
+            const json = JSON.stringify(this.state.options);
+            localStorage.setItem('options', json);
+        }
+        
     }
     componentWillUnmount() {
         console.log('componentWillUnmount');
@@ -97,6 +110,7 @@ const Options = (props) => {
     return (
         <div>
             <button onClick={props.handleDeleteOptions}>Remove All</button>
+            {props.optionList.length === 0 && <p>Please add an option to get started</p>}
             {
                 props.optionList.map((option) => (
                     <Option 
@@ -139,6 +153,10 @@ class AddOption extends React.Component {
         const error = this.props.handleAddOption(formInput);
         
         this.setState(() => ({error}));
+
+        if(!error) {
+            event.target.elements.option.value = '';
+        }
     }
 
     render() {
