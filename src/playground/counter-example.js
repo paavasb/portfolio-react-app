f74fa26c67aded14ac7e22a360f56a6c5472bad4
Paavas Bhasin <paavas22@gmail.com>
2020-04-08 00:34:59 +0530

    Lecture 46

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,9 +5,27 @@ class Counter extends React.Component {
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset = this.handleReset.bind(this);
         this.state = {
-            count: props.count,
+            count: 0,
         };
     }
+
+    componentDidMount() {
+        const stringCount = localStorage.getItem('count');
+        const count = parseInt(stringCount, 10);
+
+        if(!isNaN(count)) {
+            this.setState(() => ({count}));
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if(prevState.count !== this.state.count) {
+            //const count = JSON.stringify(this.state.count);
+            localStorage.setItem('count', this.state.count);
+        }
+    }
+
+
     handleAddOne() {
         this.setState((prevState) => {
             return {
@@ -50,7 +68,8 @@ class Counter extends React.Component {
 Counter.defaultProps = {
     count: 0,
 }
-ReactDOM.render(<Counter count={42}/>, document.getElementById('app'));
+
+ReactDOM.render(<Counter />, document.getElementById('app'));
 // let count = 0;
 // const addOne = () => {
 //     count++;
