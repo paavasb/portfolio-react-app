285e5ec63e8cc45899195d879b9a77649966d73b
Paavas Bhasin <paavas22@gmail.com>
2020-04-08 17:29:51 +0530

    Lecture 51

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log('app.js is running');
+//import './utils.js';
+// import {add, square} from './utils.js';
+
+// console.log('app.js is running');
+// console.log(square(4));
+// console.log(add(4, 5));
+
+import {isAdult, canDrink} from './person.js';
+
+console.log(isAdult(20) ? 'Adult' : 'Child');
+console.log(canDrink(20) ? 'Chug Chug' : 'Fruit Beer');
