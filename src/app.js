c207eb0f5d077bd5ee84cc8961dc12d50eb7661c
Paavas Bhasin <paavas22@gmail.com>
2020-04-08 18:45:54 +0530

    Lecture 55

--- src/app.js
+++ src/app.js
@@ -1,19 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
-const template = <p>This is JSX from Webpack</p>;
-ReactDOM.render(template, document.getElementById('app'));
-
-//import './utils.js';
-// import subtract, {add, square} from './utils.js';
-
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(4, 5));
-// console.log(subtract(100, 19));
-
-// import isSenior, {isAdult, canDrink} from './person.js';
-
-// console.log(isAdult(20) ? 'Adult' : 'Child');
-// console.log(canDrink(20) ? 'Chug Chug' : 'Fruit Beer');
-// console.log(isSenior(69) ? 'Sr.' : 'Not Sr.');
+ReactDOM.render(<IndecisionApp options={['Mars', 'Twix']}/>, document.getElementById('app'));
---
+++ src/components/AddOption.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default class AddOption extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+        this.state = {
+            error: undefined,
+        }
+    }
+    handleFormSubmit(event) {
+        event.preventDefault();
+        const formInput = event.target.elements.option.value.trim();
+        const error = this.props.handleAddOption(formInput);
+        
+        this.setState(() => ({error}));
+
+        if(!error) {
+            event.target.elements.option.value = '';
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.error && <p>{this.state.error}</p>}
+                <form onSubmit={this.handleFormSubmit}>
+                    <input type="text" name="option"></input>
+                    <button>Add Options</button>
+                </form>  
+            </div>
+        );
+    }
+}
